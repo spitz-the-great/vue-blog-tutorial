768f71849c1078370bb30df4132b7d78afa2051f
spitz-the-great <jspitzley85@gmail.com>
2018-10-26 15:31:39 -0500

    click event examples working

--- app.js
+++ app.js
@@ -4,13 +4,22 @@ new Vue({
 
         name: 'vue is linked correctly',
         website: 'https://medium.com/javascript-in-plain-english/i-created-the-exact-same-app-in-react-and-vue-here-are-the-differences-e9a1ae8077fd',
-        websiteTag: '<a href="https://medium.com/javascript-in-plain-english/i-created-the-exact-same-app-in-react-and-vue-here-are-the-differences-e9a1ae8077fd">a link</a>'
+        websiteTag: '<a href="https://medium.com/javascript-in-plain-english/i-created-the-exact-same-app-in-react-and-vue-here-are-the-differences-e9a1ae8077fd">a link</a>',
+        number: 0,
     },
 
     methods: {
         greet: function(){
             return 'sup ' + this.name
-        }
+        },
+
+        increaseNumber: function(up){
+            this.number = this.number + up;
+        },
+
+        decreaseNumber: function(down){
+            this.number = this.number - down;
+        },
            
         
     }
