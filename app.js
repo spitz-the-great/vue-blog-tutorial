6ac0272653473f31ccf30a7b2e75f2edbb2aa03b
spitz-the-great <jspitzley85@gmail.com>
2018-10-26 15:14:35 -0500

    v-html and v-bind directive examples working

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ new Vue({
     data: {
 
         name: 'vue is linked correctly',
+        website: 'https://medium.com/javascript-in-plain-english/i-created-the-exact-same-app-in-react-and-vue-here-are-the-differences-e9a1ae8077fd',
+        websiteTag: '<a href="https://medium.com/javascript-in-plain-english/i-created-the-exact-same-app-in-react-and-vue-here-are-the-differences-e9a1ae8077fd">a link</a>'
     },
 
     methods: {
